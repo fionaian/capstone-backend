6b5350d102024f2863c2756e6e221c8c87234d2f
Fiona Ian <sayhitofiona@gmail.com>
2019-08-26 10:03:06 -0500

    Added causes index and show routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
 namespace :api do
     get "/sessions" => "sessions#create"
+
+    get "/causes" => "causes#index"
+    get "/causes/:id" => "causes#show"
   end
 end
