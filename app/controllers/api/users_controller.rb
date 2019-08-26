384487ecdac289e9753bcf8f2928f9f22d6586ea
Fiona Ian <sayhitofiona@gmail.com>
2019-08-26 10:43:25 -0500

    Added PhoneToUsers column

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -3,6 +3,7 @@ class Api::UsersController < ApplicationController
     user = User.new(
       name: params[:name],
       email: params[:email],
+      phone: params[:phone],
       password: params[:password],
       password_confirmation: params[:password_confirmation]
     )
