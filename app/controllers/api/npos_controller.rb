a9925d9ef143a06b46687a16af4f85bffbbdd67c
Fiona Ian <sayhitofiona@gmail.com>
2019-08-26 10:09:50 -0500

    Added npo controller methods: index, create, show, update

--- app/controllers/api/npos_controller.rb
+++ app/controllers/api/npos_controller.rb
@@ -1,2 +1,31 @@
 class Api::NposController < ApplicationController
+  def index
+    @npos = Npo.all
+    render "index.json.jb"
+  end
+
+  def create
+    @npo = Npo.new(
+      name: params[:name],
+      cause_id: params[:cause_id],
+    )
+    if @npo.save
+      render "show.json.jb"
+    else
+      render json: { errors: @npo.errors.full_messages }, status: 422
+    end
+  end
+
+  def show
+    @npo = Npo.find_by(id: params[:id])
+    render "show.json.jb"
+  end
+
+  def update
+    @npo = Npo.find_by(id: params["id"])
+    @npo.name = params["name"] || @npo.name
+    @npo.cause_id = params["cause_id"] || @npo.cause_id
+    @npo.save
+    render "show.json.jb"
+  end
 end
